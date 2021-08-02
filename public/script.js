7e288c8f3f12ba8e7e3bd13fa16ef582e39cd2df
riki8055 <thakurritik8055@gmail.com>
2021-08-02 22:06:27 +0530

    ready to deploy

---
+++ public/script.js
@@ -0,0 +1,18 @@
+(function () {
+    var burger = document.querySelector('.navbar-burger');
+    var menu = document.querySelector('#' + burger.dataset.target);
+    burger.addEventListener('click', function () {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+})();
+
+document.addEventListener('DOMContentLoaded', () => {
+    (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+        const $notification = $delete.parentNode;
+
+        $delete.addEventListener('click', () => {
+        $notification.parentNode.removeChild($notification);
+        });
+    });
+});
