66d3c18ae74aa9657a56436338afb10f35a32cf9
riki8055 <thakurritik8055@gmail.com>
2021-12-24 13:24:50 +0530

    added port

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 - Updated from V1.0.0 to V2.0.0
 
+* Updated from V2.0.0 to V2.0.1
+
 **<a href="https://salty-reaches-73833.herokuapp.com/">Click here</a>** to go to the website.
--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const apod = require('./utils/apod')
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
