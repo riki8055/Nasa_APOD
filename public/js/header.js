8e543b6f2a7ca80d0f6339533ec31cde65c85249
riki8055 <thakurritik8055@gmail.com>
2022-07-16 10:22:00 +0530

    added animate.css to header

--- public/js/header.js
+++ public/js/header.js
@@ -1,21 +1,21 @@
-const loadingContainer = document.querySelector(".loading-container");
+// const loadingContainer = document.querySelector(".loading-container");
 const container = document.querySelector(".container");
 const navBtnOPen = document.querySelector(".nav-btn-open");
 const navBtnClose = document.querySelector(".nav-btn-close");
 
-document.onreadystatechange = () => {
-  if (document.readyState !== "complete") {
-    container.style.opacity = 0;
-    container.style.visibility = "hidden";
-    loadingContainer.style.opacity = 1;
-    loadingContainer.style.visibility = "visible";
-  } else {
-    container.style.opacity = 1;
-    container.style.visibility = "visible";
-    loadingContainer.style.opacity = 0;
-    loadingContainer.style.visibility = "hidden";
-  }
-};
+// document.onreadystatechange = () => {
+//   if (document.readyState !== "complete") {
+//     container.style.opacity = 0;
+//     container.style.visibility = "hidden";
+//     loadingContainer.style.opacity = 1;
+//     loadingContainer.style.visibility = "visible";
+//   } else {
+//     container.style.opacity = 1;
+//     container.style.visibility = "visible";
+//     loadingContainer.style.opacity = 0;
+//     loadingContainer.style.visibility = "hidden";
+//   }
+// };
 
 navBtnOPen.addEventListener("click", () => {
   container.classList.add("change");
